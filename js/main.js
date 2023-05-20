e7a0bb6d3cc79ab5b5880a801b511d07bf554b6c
Zhenya-Tureysky <108464102+Zhenya-Tureysky@users.noreply.github.com>
2023-05-20 18:51:52 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+new Swiper('.reviews__swiper', {
+	slidesPerView: 1,
+  	spaceBetween: 20,
+  	navigation: {
+    	nextEl: '.reviews__button-next',
+    	prevEl: '.reviews__button-prev',
+  	},
+});
